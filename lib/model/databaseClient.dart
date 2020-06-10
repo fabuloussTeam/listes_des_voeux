1bddae4973d8211572a9834f8c1bf6f007d9effe
fabuloussTeam <tjomemmanuel@gmail.com>
2020-06-10 13:15:29 +0100

    Creation de la classe 'item.dart' qui va implementer les element recu de la BDD. Creation de la base de donne client 'databaseClient.dart'

---
+++ lib/model/databaseClient.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+// Creation de la base de donne et notre table: NB: dart utilise SQL
+class DatabaseClient {
+
+  Database _database;
+
+  Future<Database> get database async {
+    if(_database != null) {
+      return _database;
+    } else {
+      // Creer cette database si c'est null
+      return create();
+    }
+  }
+
+  // Creation de la DB:
+  // recuperation du dossier racine ("directory") de la BD avec la librairie  "path_provider"
+  Future create() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String database_directory = join(directory.path, 'database.db'); // librairie path.dart
+    var bdd = await openDatabase(database_directory, version: 1, onCreate: _onCreate);
+    return bdd;
+  }
+
+  // Creation de la table 'item' dans la BDD
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE item (
+      id INTEGER PRIMARY KEY,
+      nom TEXT NOT NULL) 
+    ''');
+  }
+
+
+}
---
+++ lib/model/item.dart
@@ -0,0 +1,21 @@
+class Item {
+
+  int id;
+  String nom;
+
+  Item();
+
+  void fromMap(Map<String, dynamic> map){
+    this.id = map['id'];
+    this.nom = map['nom'];
+  }
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = {
+      'nom': this.nom, //'nom' ici c'est map['nom']
+    };
+    if (id != null){
+      map['id'] = this.id;
+    }
+  }
+}
